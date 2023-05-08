bfecf433508886fc9dc3876312c2529fd6682e41
RodCato <code@eliment.org>
2023-05-08 10:35:33 -0500

    Week 3 Assesment Complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,57 +11,204 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+ //Pseudo Code:
+ //input number greater than 2
+ //output array of numbers from fibonacci series.
+ //Create a function named fibFunction that will take a number, create an array through iteration equal to that number, and return a series of numbers from the fibonacci sequence
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
-const fibLength2 = 10
+describe("fibFunction", () => {
+  it("takes in a number (greater than 2) and returns an array", () => {
+    const fibLength1 = 6
+    // Expected output: [1, 1, 2, 3, 5, 8]
+    expect(fibFunction(fibLength1)).toStrictEqual([1, 1, 2, 3, 5, 8])
+    const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(fibFunction(fibLength2)).toStrictEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+}) 
+
+// FAIL  ./code-challenges.test.js
+// fibFunction
+//   ✕ takes in a number (greater than 2) and returns an array
+
+// ● fibFunction › takes in a number (greater than 2) and returns an array
+
+//   ReferenceError: fibFunction is not defined
+
 
 // b) Create the function that makes the test pass.
+
+
+
+const fibFunction = (input) =>{
+  if (input < 3) {
+      return "Input must be larger than 2";
+    }
+    let fibFuncArray = [1, 1];
+    for (let i = 2; i < input; i++) {
+      fibFuncArray[i] = fibFuncArray[i-1] + fibFuncArray[i-2];
+    }
+    return fibFuncArray;
+  }
+
+// console.log(fibFunction(fibLength1))
+ // output: [1, 1, 2, 3, 5, 8]
+// console.log(fibFunction(fibLength2))
+// output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+// PASS  ./code-challenges.test.js
+// fibFunction
+//   ✓ takes in a number (greater than 2) and returns an array (1 ms)
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
+//Pseudo Code: 
+//input is an object
+//return an array of values from least to greatest    
+//Create a function named leastToGreatest that sorts an object to create an array 
+//of values from least to greatest and return that array.
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+  describe("leastToGreatest", () => {
+    it("takes in an object and returns an array of the values from least to greatest", () => {
+      const studyMinutesWeek1 = {
+        sunday: 90,
+        monday: 30,
+        tuesday: 20,
+        wednesday: 15,
+        thursday: 30,
+        friday: 15,
+        saturday: 60
+      }
+      // Expected output: [15, 15, 20, 30, 30, 60, 90]
+     expect(leastToGreatest(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+      const studyMinutesWeek2 = {
+       sunday: 100,
+       monday: 10,
+       tuesday: 45,
+       wednesday: 60,
+       thursday: 20,
+       friday: 15,
+       saturday: 65
+     }
+      // Expected output: [10, 15, 20, 45, 60, 65, 100]
+    expect(leastToGreatest(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+  })
+  // FAIL  ./code-challenges.test.js
+  // fibFunction
+  //   ✓ takes in a number (greater than 2) and returns an array (1 ms)
+  // leastToGreatest
+  //   ✕ takes in an object and returns an array of the values from least to greatest
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+  // ● leastToGreatest › takes in an object and returns an array of the values from least to greatest
+
+  //   ReferenceError: leastToGreatest is not defined
+
+
 
 // b) Create the function that makes the test pass.
+
+const leastToGreatest = (input) => {
+  const minsArray = Object.values(input) 
+  minsArray.sort((a,b) => a - b)
+  return minsArray
+}
+//console.log(leastToGreatest(studyMinutesWeek1))
+// output: [15, 15, 20, 30, 30, 60, 90]
+//console.log(leastToGreatest(studyMinutesWeek2))
+// output: [10, 15, 20, 45, 60, 65, 100]
+
+// PASS  ./code-challenges.test.js
+// fibFunction
+//   ✓ takes in a number (greater than 2) and returns an array (1 ms)
+// leastToGreatest
+//   ✓ takes in an object and returns an array of the values from least to greatest
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.15 s
+// Ran all test suites.
+// ✨  Done in 0.73s.
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+//input: is an array
+//output: is an array of the sum of all the numbers in the array
+//Create a function named accumulateSum that takes in an array and returns an array of the sum of all the numbers in the array. If array “empty” then return an empty array.
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+describe("accumulateSum", () => {
+  it("takes in an array and returns an array", () => {
+    // a)
+    expect(accumulateSum(accountTransactions1)).toEqual([100,  83, 60, 51])
+    // const accountTransactions1 = [100, -17, -23, -9]
+                 // Expected output: [100, 83, 60, 51]
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+    expect(accumulateSum(accountTransactions2)).toEqual([250, 161, 261, 165])
+    // const accountTransactions2 = [250, -89, 100, -96]
+    // Expected output: [250, 161, 261, 165]
+    expect(accumulateSum(accountTransactions3)).toEqual([]);
+    // const accountTransactions3 = []
+    // Expected output: []
 
-const accountTransactions3 = []
-// Expected output: []
+  })
+  })
 
-// b) Create the function that makes the test pass.
+  // FAIL  ./code-challenges.test.js
+  // fibFunction
+  //   ✓ takes in a number (greater than 2) and returns an array (1 ms)
+  // leastToGreatest
+  //   ✓ takes in an object and returns an array of the values from least to greatest (1 ms)
+  // accumulateSum
+  //   ✕ takes in an array and returns an array
+
+  // ● accumulateSum › takes in an array and returns an array
+
+  //   ReferenceError: accumulateSum is not defined
+
+  const accountTransactions1 = [100, -17, -23, -9]
+  const accountTransactions2 = [250, -89, 100, -96]
+  const accountTransactions3 = []
+// b) Create the function that makes the test pass
+
+const accumulateSum = (array) => {
+  if(array.length === 0)
+   return []
+   let sum = 0  
+   return array.map((value) => {
+    sum += value
+    return sum    
+   })
+  }
+
+  // console.log(accumulateSum(accountTransactions1))
+  // // output: [100, 83, 60, 51]
+  // console.log(accumulateSum(accountTransactions2))
+  // //output: [250, 161, 261, 165]
+  // console.log(accumulateSum(accountTransactions3))
+  // //output: []
+
+// PASS  ./code-challenges.test.js
+// fibFunction
+//   ✓ takes in a number (greater than 2) and returns an array (2 ms)
+// leastToGreatest
+//   ✓ takes in an object and returns an array of the values from least to greatest
+// accumulateSum
+//   ✓ takes in an array and returns an array (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.144 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.63s.
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: Jsx is a hybrid language of javascript and html used in react components.
 
-Researched answer:
+Researched answer: JSX is a way of writing code in JavaScript that lets you define what a web page should look like, similar to how you write HTML. It's used in React, which is a library for making web pages that change and update in response to user actions. It allows you to write HTML-like syntax within JavaScript code.
 
 1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager made by Facebook. It can be used to install packages and manage dependencies. 
 
-Researched answer:
+Researched answer: Yarn is a package manager and project manager developed by Facebook. It installs dependencies listed in the package.json file that are stored in a local directory called node-modules. It also creates a yarn.lock file to ensure all developers working on a project are using the same versions of the dependencies.
 
-3. What is a React component?
+3. What is a React component? 
 
-Your answer:
+Your answer: A React component is piece of code that represents “something” in your application. Many components come together to make up one web application. Each component is a separate file.
 
-Researched answer:
+Researched answer: A React component is a modular, reusable piece of code that is used to build web pages. You can define a component using either a class or a function. A component takes in properties (props) as inputs and returns a React element that describes what should be displayed on the screen.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State is a property of a component and can change over time. Props are a way to pass data between components.
 
-Researched answer:
+Researched answer: State is defined and managed within a component and can only be accessed and modified from within that component. Props, are passed down from a parent component to its child components. They are used to provide information to a component, such as configuration or data to display.
 
-5. What is the DOM?
+5. What is the DOM? 
 
-Your answer:
+Your answer: DOM Is a method that allows for interaction wit a document displayed in a web browser.
 
-Researched answer:
+Researched answer: Document Object Model provides a set fo methods and properties that can be used to manipulate a document. Using the DOM, web developers can create dynamic and interactive web pages that respond to user input and update content in real-time.
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: Div tags are used for big portions of content and span tags are used for smaller pieces of content.
 
-Researched answer:
+Researched answer: Div tag is used to group HTML elements together is commonly used to define sections of a web page and apply styles or other attributes to a group of elements. Span tag is used to group inline HTML elements together and is commonly used to apply styles or other attributes to a specific section of text or other inline elements. 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object-oriented programming is when data and functions are bundled together into objects, which are self-contained and have a state and behavior. Each object can communicate with other objects, and its behavior can be controlled through the manipulation of its data.
 
-2. Ruby:
+2. Ruby: Ruby is a high-level, general-purpose programming language. In Ruby, everything is an object, including data types like strings and numbers. This means that Ruby provides a rich set of built-in classes and methods that can be used to manipulate these objects.
 
-3. Implicit return:
+3. Implicit return: A function returns its value by default.
 
-4. Ruby blocks:
+4. Ruby blocks: In Ruby, a block is a piece of code that can be passed to a method as an argument. When a block is passed to a method, the method can yield control to the block, which can then execute its code and return control to the method. It is commonly used for iterating over arrays and hashes.
 
-5. Ruby hashes:
+5. Ruby hashes: A Ruby hash is a data structure that stores data in key-value pairs. It is similar to an array, but instead of using integer indices to access elements, a hash uses keys, which can be any object.
